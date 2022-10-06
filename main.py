15c112dad79dac03fcb039730a37dd2185ffd3c0
Max Schild <max@deintv.de>
2022-10-06 21:54:57 +0200

    added requirements.txt

--- main.py
+++ main.py
@@ -9,11 +9,16 @@ import motor.motor_asyncio
 
 app = FastAPI()
 
-username = "mongo"
-password = "mongo"
-mongo_db_connection_string = "mongodb+srv://" \
+username = "outdoor_insights" #"mongo"
+password = "P6mItv39dblb4WF9" #"mongo"
+"""mongo_db_connection_string = "mongodb+srv://" \
                              "{username}:{password}@cluster0.gvyd35q.mongodb.net/?retryWrites=true" \
+                             "&w=majority".format(username=username, password=password)"""
+mongo_db_connection_string = "mongodb+srv://" \
+                             "{username}:{password}@cluster0.olpftqz.mongodb.net/?retryWrites=true" \
                              "&w=majority".format(username=username, password=password)
+
+#mongodb+srv://outdoor_insights:P6mItv39dblb4WF9@cluster0.olpftqz.mongodb.net/?retryWrites=true&w=majority
 #client = pymongo.MongoClient(mongo_db_connection_string)
 
 #client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
@@ -32,7 +37,8 @@ async def root():
     #entry = await collection.find_one()
     #entry = await db["students"].find_one().to_list(1000)
     #entry = await db["companies"].find_one()
-    entry = await db["companies"].find().to_list(1000)
+    #entry = await db["companies"].find().to_list(1000)
+    entry = await db["test"].find().to_list(1000)
     print(entry)
 
     return {"message": "Hello World"}
